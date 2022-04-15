0f33250a16079b2d20e606b9c446a05ba628530d
keksas <k.rimantas@gmail.com>
2022-04-15 19:13:13 +0300

    6D array solution

--- sum.js
+++ sum.js
@@ -18,6 +18,47 @@ const c = [
   -Infinity,
   10,
 ];
+const d = [
+  1,
+  2,
+  3,
+  [4],
+  [5],
+  [6],
+  [7, 8],
+  [9, 10],
+  [
+    1,
+    2,
+    3,
+    [4],
+    [5],
+    [6],
+    [7, 8],
+    [9, 10],
+    [
+      1,
+      2,
+      3,
+      [4],
+      [5],
+      [6],
+      [7, 8],
+      [9, 10],
+      [
+        1,
+        2,
+        3,
+        [4],
+        [5],
+        [6],
+        [7, 8],
+        [9, 10],
+        [1, 2, 3, [4], [5], [6], [7, 8], [9, 10]],
+      ],
+    ],
+  ],
+];
 
 function isGoodNumber(n) {
   return typeof n === "number" && isFinite(n);
@@ -26,21 +67,51 @@ function isGoodNumber(n) {
 function sum(list) {
   let total = 0;
 
-  for (const n of list) {
-    if (Array.isArray(n)) {
-      for (const m of n) {
-        if (!isGoodNumber(m)) {
+  for (const n1 of list) {
+    if (Array.isArray(n1)) {
+      for (const n2 of n1) {
+        if (Array.isArray(n2)) {
+          for (const n3 of n2) {
+            if (Array.isArray(n3)) {
+              for (const n4 of n3) {
+                if (Array.isArray(n4)) {
+                  for (const n5 of n4) {
+                    if (Array.isArray(n5)) {
+                      for (const n6 of n5) {
+                        if (!isGoodNumber(n6)) {
+                          continue;
+                        }
+                        total += n6;
+                      }
+                    }
+                    if (!isGoodNumber(n5)) {
+                      continue;
+                    }
+                    total += n5;
+                  }
+                }
+                if (!isGoodNumber(n4)) {
+                  continue;
+                }
+                total += n4;
+              }
+            }
+            if (!isGoodNumber(n3)) {
+              continue;
+            }
+            total += n3;
+          }
+        }
+        if (!isGoodNumber(n2)) {
           continue;
         }
-        total += m;
+        total += n2;
       }
     }
-
-    if (!isGoodNumber(n)) {
+    if (!isGoodNumber(n1)) {
       continue;
     }
-
-    total += n;
+    total += n1;
   }
 
   return total;
@@ -54,3 +125,6 @@ console.log("AREA", r2, "-->", 51);
 
 const r3 = sum(c);
 console.log("AREA", r3, "-->", 51);
+
+const r4 = sum(d);
+console.log("AREA", r4, "-->", 275);
